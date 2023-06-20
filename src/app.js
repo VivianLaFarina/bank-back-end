e73883f48f9771186f3fc70a6271733f9543b288
Vivian La Farina <vivianlafarina@gmail.com>
2023-06-20 16:33:09 -0500

    main config-dev mode

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// routes
+
+module.exports = app;
